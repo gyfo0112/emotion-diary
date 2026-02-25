681f600b3e028b4b8e2e2f28d41ffe3a5bfb4a1c
12 <gyfo6474@naver.com>
2026-02-25 10:05:25 +0900

    update

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Viewer from "../components/Viewer";
+import { getStringedDate } from "../util/getStringedDate";
+import useDiary from "../hooks/useDiary";
+import useTitle from "../hooks/useTitle";
 
 const Diary = () => {
+  const { id } = useParams();
+  const nav = useNavigate();
+  const curDiaryItem = useDiary(id);
 
-    const params = useParams()
-    console.log(params)
+  useTitle(`${id}번의 일기 보기`);
+
+  if (!curDiaryItem) {
+    return <div>로딩 중...</div>;
+  }
+
+  const { createdDate, emotionId, content } = curDiaryItem;
+  const title = getStringedDate(new Date(createdDate));
+
   return (
-    <div>{params.id}번 Diary</div>
-  )
-}
+    <div>
+      <Header
+        leftChild={<Button text={"뒤로가기"} onClick={() => nav(-1)} />}
+        title={`${title} 기록`}
+        rightChild={
+          <Button
+            text={"수정하기"}
+            type={"POSITIVE"}
+            onClick={() => nav(`/edit/${id}`)}
+          />
+        }
+      />
+      <Viewer emotionId={emotionId} content={content} />
+    </div>
+  );
+};
 
-export default Diary
+export default Diary;
